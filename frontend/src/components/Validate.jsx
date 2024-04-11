1d5168f8864b2a20f8f1928bc4f47640d05e97d7
2171001 <abelbenedict20@gnu.ac.in>
2024-04-11 14:44:30 +0530

    Updated Validate.jsx

--- frontend/src/components/Validate.jsx
+++ frontend/src/components/Validate.jsx
@@ -1,14 +1,25 @@
+import React from 'react';
+import Axios from 'axios';
 import AdminNavbar from "./AdminNavbar";
+
 const Validate = () => {
-  
+	const handleValidate = async () => {
+		try {
+			// Make an HTTP POST request to the backend endpoint
+			const response = await Axios.post('/api/validateCertificate', { certificateId: 'certificateIdHere' });
+			console.log(response.data);
+		} catch (error) {
+			console.error('Error validating certificate:', error);
+		}
+	};
 
-    return (
-        <div >
-          <AdminNavbar />
-            <h1 className="text-center px-32">VALIDATE</h1>
-
-        </div>
-    );
+	return (
+		<div>
+			<AdminNavbar />
+			<h1 className="text-center px-32">VALIDATE CERTIFICATES</h1>
+			<button onClick={handleValidate}>Validate Certificate</button>
+		</div>
+	);
 };
 
 export default Validate;
