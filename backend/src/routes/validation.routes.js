f2b49111ae582edaa44bd4505ba9b3d7a27c003c
2171001 <abelbenedict20@gnu.ac.in>
2024-04-16 13:51:33 +0530

    modified:   backend/src/routes/validation.routes.js

--- backend/src/routes/validation.routes.js
+++ backend/src/routes/validation.routes.js
@@ -1,23 +1,36 @@
 const express = require("express");
 const router = express.Router();
-const { exec } = require('child_process');
 
 router.post('/validateCertificate', async (req, res) => {
-	try {
-		// Execute the validation script
-		exec('python ../../Validation_Script.py', (error, stdout, stderr) => {
-			if (error) {
-				console.error(`Error executing validation script: ${erro}`);
-				res.status(500).json({ success: false, message: 'Internal server error' });
-			} else {
-				console.log(`Validation script output: ${stdout}`);
-				res.status(500).json({ success: true, message: 'Validation successful' });
-			}
-		});
-	} catch (error) {
-		console.error(`Error in validation route: ${error}`);
-		res.status(500).json({ success: false, message: 'Internal server error' });
-	}
+    try {
+        const { certificateId } = req.body;
+        const { exec } = require('child_process');
+        const scriptPath = '../../Validation_Script.py';
+        const scriptCommand = `python ${scriptPath} ${certificateId}`;
+        const { error, stdout } = await runCommand(scriptCommand);
+        if (error) {
+            console.error(`Error executing validation script: ${error}`);
+            res.status(500).json({ success: false, message: 'Internal server error' });
+        } else {
+            console.log(`Validation script output: ${stdout}`);
+            res.status(200).json({ success: true, message: 'Validation successful' });
+        }
+    } catch (error) {
+        console.error(`Error in validation route: ${error}`);
+        res.status(500).json({ success: false, message: 'Internal server error' });
+    }
 });
+
+async function runCommand(command) {
+    return new Promise((resolve, reject) => {
+        exec(command, (error, stdout) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve({ error, stdout });
+            }
+        });
+    });
+}
 
 module.exports = router;
